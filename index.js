0d2d30c675a2fe7d31f65be5e162702ebe4eec31
casey bradford <caseyebradford@gmail.com>
2020-04-23 23:37:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,24 +1,4 @@
 const app = "I don't do much."
 
-var dodger = document.getElementById('dodger')
-
-
-function moveDodgerLeft() {
-  var leftNumbers = dodger.style.left.replace('px', '')
-  var left = parseInt(leftNumbers, 10)
-  if (left > 0) {
-    dodger.style.left = `${left - 1}px`
-  }
-}
-
-$(document).ready(function(){
-
-document.addEventListener('keydown', function(e) {
-  if (e.which === 37) {
-    moveDodgerLeft()
-  }
-})
-
-});
 
 
